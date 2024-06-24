64465d520d099d627b762f6fa0e42e3d504b4aad
Matheus Gomes <matheusbgomes@hotmail.com>
2024-06-24 14:27:24 -0300

    refactor: add main function to manage form operations

---
+++ main.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const axios = require('axios')
+const getSourceFormData = require('./getSourceFormData')
+const createFormTemplate = require('./createFormTemplate')
+const duplicateForm = require('./duplicateForm')
+
+async function main() {
+  const formId = 'flpHU7hr';
+  const newformTitle = 'Formulario Teste 456';
+  const data = await getSourceFormData(formId);
+  const formTemplate = createFormTemplate(data);
+  await duplicateForm(formTemplate, newformTitle)
+}
+
+main()
+
